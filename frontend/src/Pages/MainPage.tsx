dbed73379062c112a5d95520e617fdd297eeda5c
Rana <ranamohamad215@gmail.com>
2024-11-25 23:12:58 +0200

    Added Initial Setup of the main page.

---
+++ frontend/src/Pages/MainPage.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material"
+import SideBar from "../Components/SideBar"
+
+function MainPage() {
+  return (
+    <Box>
+        {/* Side drawer */}
+        <SideBar/>
+
+    </Box>
+  )
+}
+
+export default MainPage
