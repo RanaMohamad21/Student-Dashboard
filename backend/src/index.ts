a28ae6d560f0a31f523844ef2a39955d4597757f
Rana <ranamohamad215@gmail.com>
2024-11-26 15:28:04 +0200

    Finished the Server setup.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,49 @@
 import express from 'express';
-import mongoose from 'mongoose';
+// import bodyParser from 'body-parser';
+import dotenv from 'dotenv'
+import morgan from 'morgan';
 import cors from 'cors';
+import helmet from 'helmet';
+import connectDB  from './config/db';
+import getErrorMessage from './utils/getErrorMessage';
 
+
+//* Configuration
+const http = require('http');
+dotenv.config() 
 const app = express();
 const port = process.env.BACKEND_PORT || 5000;
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET','POST','PUT','DELETE']
-}))
+// middleware
+app.use(cors());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy:'cross-origin'})); // To allow requests from other servers.
+app.use(morgan("common"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// DB connection and Server Initialization:
+
+const server = http.createServer(app);
+const startServer = async () => {
+    try {
+        await connectDB();
+        server.listen(port,()=>{
+            console.log(`Server is running on port ${port}`)
+        })
+        
+    } catch (error) {
+        getErrorMessage(error);
+        process.exit(1); // Exit with failure code
+    }
+}
+startServer();
+
+// Routes
+// app.use()
+
+
+
+
