d3fc0099419cc1a3eb4fc8a60cab290f2d366399
Rana <ranamohamad215@gmail.com>
2024-11-26 15:55:53 +0200

    Finalized Student Schema.

--- backend/src/models/students.ts
+++ backend/src/models/students.ts
@@ -1,5 +1,26 @@
+import { mongo } from "mongoose";
+
 const mongoose = require("mongoose");
 
 const studentSchema = new mongoose.Schema({
-    
-})
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // References the 'User' collection, representing the user associated with the student (could be a student or teacher)
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true, // Name of the student, required field
+  },
+  age: {
+    type: Number,
+    required: true, // Age of the student, assuming the school enforces age restrictions based on grade
+  },
+  grade: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Grade", // References the 'Grade' collection, representing the student's grade level
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Student", studentSchema);
