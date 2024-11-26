1bf22c9602f98370db42c03854abd9d8980a368d
Rana <ranamohamad215@gmail.com>
2024-11-26 20:28:57 +0200

    Finalized the subject schema.

---
+++ backend/src/models/subjects.ts
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const subjectSchema = new mongoose.Schema({
+    subjectName:{
+        type: String,
+        required: true
+    },
+    SubjectCode:{
+        type: String,
+        required: true,
+        unique: true // Ex: "Math01"
+    },
+      classroom:{
+        type: String,
+        required: false
+      },
+      schedule:{
+        type: String,
+        required: false
+      },
+      
+    assignedTeacher: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Teacher",
+      },
+      
+});
+
+// For faster queries
+subjectSchema.index({subjectName:1});
+subjectSchema.index({assignedGrade:1});
+subjectSchema.index({assignedTeacher:1});
+
+
+module.exports = mongoose.model("Subject", subjectSchema);
