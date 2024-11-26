6e5d19911c7a3b193b6138fa9218fe2ecc26a6e1
Rana <ranamohamad215@gmail.com>
2024-11-26 20:28:47 +0200

    Finalized the students schema.

--- backend/src/models/students.ts
+++ backend/src/models/students.ts
@@ -1,17 +1,18 @@
-import { mongo } from "mongoose";
 
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
 
 const studentSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User", // References the 'User' collection, representing the user associated with the student (could be a student or teacher)
     required: true,
+    unique: true
   },
   name: {
     type: String,
     required: true, // Name of the student, required field
-  },
+    minlength: [2,"Name must be at least 2 characters long."]
+},
   age: {
     type: Number,
     required: true, // Age of the student, assuming the school enforces age restrictions based on grade
@@ -20,7 +21,12 @@ const studentSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "Grade", // References the 'Grade' collection, representing the student's grade level
     required: true,
+    unique: true
   },
+  subjects:[{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Subject"
+  }]
 });
 
 module.exports = mongoose.model("Student", studentSchema);
