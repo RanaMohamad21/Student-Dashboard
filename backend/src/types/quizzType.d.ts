ac99594f6a082a986b102a28012abee09b8c33ca
Rana <ranamohamad215@gmail.com>
2024-11-29 21:43:42 +0200

    Added Quizzes types.

--- backend/src/types/quizzType.d.ts
+++ backend/src/types/quizzType.d.ts
@@ -1,28 +1,28 @@
 import mongoose from "mongoose";
 
 interface Question {
-    questionText: string;
-    answers: string[];
-    correctAnswer: string;
+  questionText: string;
+  answers: string[];
+  correctAnswer: string;
 }
 
-interface Submission{
-    student: mongoose.Schema.Types.ObjectId;
-    score: number;
-    date: Date;
+interface Submission {
+  student: mongoose.Schema.Types.ObjectId;
+  score: number;
+  date: Date;
 }
 
-interface Schedule{
-    start: Date;
-    end: Date;
+interface Schedule {
+  start: Date;
+  end: Date;
 }
 
-export interface Quiz extends Document {
-    subject: mongoose.Schema.Types.ObjectId;
-    title: string;
-    questions: Question[];
-    submissions: submissions[];
-    schedule: Schedule;
-    status: "active" | "inactive" | "archived";
-    maxScore: number;
+export interface QuizType extends Document {
+  subject: mongoose.Schema.Types.ObjectId;
+  title: string;
+  questions: Question[];
+  submissions: submissions[];
+  schedule: Schedule;
+  status: "active" | "inactive" | "archived";
+  maxScore: number;
 }
